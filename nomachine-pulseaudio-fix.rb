4416423939f456b47c4461b7b7226bb1cf7c49e0
Nick M Cummins <nickmcummins@gmail.com>
2016-05-17 11:35:46 -0700

    Update nomachine-pulseaudio-fix.rb

--- nomachine-pulseaudio-fix.rb
+++ nomachine-pulseaudio-fix.rb
@@ -5,7 +5,7 @@ NX_LOG = '/usr/NX/var/log/nxserver.log'
 def extract_nxdevice_dir_from_log
   log = File.readlines(NX_LOG)
   native_socket_filepath = log.select{|line| line.include?('native.socket')}.last.split(' ').last
-  native_socket_filepath[0..native_socket_filepath - 2]
+  native_socket_filepath[0..native_socket_filepath.size - 2]
 end
 
 uid = Process::UID.from_name(ARGV[0])
