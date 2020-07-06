26c07219516d9734471d4f93df705533ec59c127
nickmcummins <nickmcummins@gmail.com>
2020-07-06 15:42:44 -0700

    DisjointSets of dependency trees

--- archlinux/pkgbuilds/dependency_resolver.py
+++ archlinux/pkgbuilds/dependency_resolver.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-from package_node import PackageNode
+from disjointsets import DisjointSets
+from node import Node
 from pkgbuild_parser import parse_file
 
 packages_by_name = {}
@@ -11,7 +12,7 @@ def lookup_or_create_package_node(pkgname):
     if pkgname in packages_by_name.keys():
         package = packages_by_name[pkgname]
     else:
-        package = PackageNode(pkgname)
+        package = Node(pkgname)
         packages_by_name[pkgname] = package
     return package
 
@@ -22,12 +23,10 @@ if __name__ == '__main__':
     provides = list(map(lambda pkgname: pkgname.replace('-git', '').replace('-hg', '').replace('-svn', ''), pkgdirs))
     pkgbuild_pkgdirs = list(filter(lambda path: os.path.isdir(path) and 'PKGBUILD' in os.listdir(path), map(lambda filepath: f'{parentdir}/{filepath}', pkgdirs)))
     for pkgdir in pkgbuild_pkgdirs:
-        print(pkgdir)
         try:
             pkgbuild = dict(parse_file(open(f'{pkgdir}/PKGBUILD')))
             pkg = lookup_or_create_package_node(pkgbuild['pkgname'])
             if 'depends' in pkgbuild.keys() and len(pkgbuild['depends']) > 0:
-                print(pkgbuild['depends'])
                 for dependency in filter(lambda depend: depend in provides, pkgbuild['depends']):
                     pkg.add_dependency(lookup_or_create_package_node(dependency))
             else:
@@ -36,4 +35,7 @@ if __name__ == '__main__':
         except Exception as ae:
             print(ae)
 
-
+    print('\n'.join(map(lambda pkg: str(pkg), packages_by_name.values())))
+    ds = DisjointSets(packages_by_name.values())
+    disjointed_sets = ds.get_sets()
+    print(disjointed_sets)
