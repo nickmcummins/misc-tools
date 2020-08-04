5cd8cb114d545c32cbc4c1d588dd2367d69ef7c2
nickmcummins <nickmcummins@gmail.com>
2020-08-04 15:15:27 -0700

    rkcommon_version_from_cmake_project_def.py: simplify index lookup

--- cmake/rkcommon_version_from_cmake_project_def.py
+++ cmake/rkcommon_version_from_cmake_project_def.py
@@ -1,8 +1,4 @@
 cmakelists = open('CMakeLists.txt', 'r')
 cmakelists_lines = cmakelists.read().split('\n')
 projectdef = list(filter(lambda line: line.startswith('project'), cmakelists_lines))[0].split(' ')
-
-for i in range(0, len(projectdef)):
-    if projectdef[i] == 'VERSION':
-        projectversion = projectdef[i + 1]
-print(projectversion)
+print(projectdef[projectdef.index('VERSION') + 1])
