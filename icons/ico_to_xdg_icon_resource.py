200a123fef4f315462b2c685c73620bae81d530c
Nick M Cummins <nickmcummins@gmail.com>
2025-10-11 18:29:43 -0700

    ico_to_xdg_icon_resource.py - for installing an .ico in Linux using xdg-icon-resource install

---
+++ icons/ico_to_xdg_icon_resource.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from os import popen, path
+
+
+def run(cmd):
+    print(f'Running {cmd} ...')
+    output = popen(cmd).read()
+    return output
+
+def pngsize(pngfile):
+    return pngfile.split('_')[-1].split('x')[0]
+
+
+if __name__ == '__main__':
+    icofile = sys.argv[1]
+    iconname = icofile.replace(".ico", "").replace("-", " ")
+
+    if path.exists('tmp'):
+        run('rm -rf tmp')
+
+    os.mkdir('tmp')
+
+    run(f'icotool -x  {icofile} -o tmp')
+
+    pngfiles = os.listdir('tmp')
+
+    for pngfile in pngfiles:
+        #print(pngfile)
+        run(f'xdg-icon-resource install --novendor --size {pngsize(pngfile)} tmp/{pngfile} "{iconname}"')
