ee8a1c5244674957ebb627ec028f760bfdf398c4
Nick M Cummins <nickmcummins@gmail.com>
2025-12-30 16:34:24 -0800

    Create tcpvcon_continuous.py

---
+++ tcpvcon_continuous.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime
+
+EXCLUDE_PROCESSES = {'nxplayer.bin', 'X410.exe', 'WinStore.App.exe', 'nxrunner.bin', 'GitHubDesktop.exe', 'OUTLOOK.EXE',
+                     'adb.exe', 'pycharm64.exe', 'mDNSResponder.exe', 'MouseWithoutBorders.exe', 'firefox.exe',
+                     'cef_server.exe', 'LockApp.exe', 'msrdc.exe', 'full-line-inference.exe', 'MsMpEng.exe',
+                     'MsTeamsVdi.exe'}
+
+class TCPConnection:
+    def __init__(self, timestamp, line):
+        cols = line.split(',')
+        self.timestamp = timestamp
+        self.protocol = cols[0]
+        self.process_name = cols[1]
+        self.pid = cols[2]
+        self.state = cols[3]
+        self.local = cols[4]
+        self.remote = cols[5]
+
+    def __str__(self):
+        return f'{self.timestamp},{self.protocol},{self.process_name},{self.pid},{self.state},{self.local},{self.remote}'
+
+
+def include_line(tcp_connection):
+    if EXCLUDE_PROCESSES.__contains__(tcp_connection.process_name):
+        return False
+    return True
+
+with open('tcpvcon.log', 'a') as log:
+    while True:
+        timestamp = datetime.now()
+        tcp_connection_csvs = list(filter(lambda line: len(line.strip()) > 0, os.popen('tcpvcon64.exe -c -a -nobanner').read().split('\n')))
+        tcp_connections = list(filter(lambda tcpconnection: include_line(tcpconnection), map(lambda line: TCPConnection(timestamp, line), tcp_connection_csvs)))
+        log.write('\n'.join(map(lambda tcp: str(tcp), tcp_connections)) + '\n')
