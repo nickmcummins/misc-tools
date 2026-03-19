26a5d683dd0649386e3611da583bd9445abccfdc
Nick M Cummins <nickmcummins@gmail.com>
2026-03-19 13:04:33 -0700

    desktop2lnk.py - use argparse

--- icons/desktop2lnk.py
+++ icons/desktop2lnk.py
@@ -1,7 +1,8 @@
-import os
+#!/usr/bin/env python
 
+import os
+import argparse
 import desktop_entry_lib
-import sys
 
 WSL_EXE = 'C:\Windows\System32\wsl.exe'
 
@@ -14,26 +15,36 @@ def get_wsl_distribution_name():
 
 linux_distribution_name = get_wsl_distribution_name()
 
-def wsl_exe_run_args(linux_cmd):
+def wsl_exe_run_args(linux_cmd, shellcmd):
     linux_cmd = linux_cmd.replace('"', '')
-    return f'-d {linux_distribution_name} /bin/zsh -lc \'nohup {linux_cmd} >/dev/null 2>&1 & sleep 1\''
+    return f'-d {linux_distribution_name} {shellcmd} -lc \'nohup {linux_cmd} >/dev/null 2>&1 & sleep 1\''
 
-def wsl_windows_default_lnk_folder():
-    return f'/mnt/c/Users/nickm/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/{linux_distribution_name}'
+def wsl_windows_default_lnk_folder(user):
+    return f'/mnt/c/Users/{user}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/{linux_distribution_name}'
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='desktop2lnk', description='Converts a Linux destop entry (.desktop) in a WSL instance to Windows shortcut.')
+    parser.add_argument('desktop_entry_file')  # positional argument
+    parser.add_argument('--outdir', default=None)
+    parser.add_argument('--shellcmd', default='/bin/zsh')
+    parser.add_argument('--verbose', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--user')
+    args = parser.parse_args()
 
-desktop_entry_file = sys.argv[1]
-windows_lnk_file = os.path.basename(desktop_entry_file.replace('.desktop', '.lnk'))
-windows_link_outdir = sys.argv[2] if len(sys.argv) > 2 else os.path.dirname(desktop_entry_file)
-print(windows_link_outdir)
+    desktop_entry_file = args.desktop_entry_file
+    windows_lnk_file = os.path.basename(desktop_entry_file.replace('.desktop', '.lnk'))
+    windows_link_outdir = args.outdir if args.outdir is not None else os.path.dirname(desktop_entry_file)
 
-entry = desktop_entry_lib.DesktopEntry.from_file(desktop_entry_file)
+    print(windows_link_outdir)
 
-print("Name: " + entry.Name.default_text)
-print("Comment: " + entry.Comment.default_text)
-print("Type: " + entry.Type)
-print("Exec: " + entry.Exec)
-print("Icon: " + entry.Icon)
+    entry = desktop_entry_lib.DesktopEntry.from_file(desktop_entry_file)
 
-working_dir_arg = f'-w {entry.Path}' if entry.Path is not None else f"\\\\wsl.localhost\\{linux_distribution_name}\\home"
-print(f'mslink -l "{WSL_EXE}" -n "{entry.Comment}" -w "{working_dir_arg}" -o "{wsl_windows_default_lnk_folder()}/{windows_lnk_file}" -a "{wsl_exe_run_args(entry.Exec)}"')
+    if args.verbose:
+        print("Name: " + entry.Name.default_text)
+        print("Comment: " + entry.Comment.default_text)
+        print("Type: " + entry.Type)
+        print("Exec: " + entry.Exec)
+        print("Icon: " + entry.Icon)
+
+    working_dir_arg = f'-w {entry.Path}' if entry.Path is not None else f"\\\\wsl.localhost\\{linux_distribution_name}\\home"
+    print(f'mslink -l "{WSL_EXE}" -n "{entry.Comment}" -w "{working_dir_arg}" -o "{wsl_windows_default_lnk_folder(args.user)}/{windows_lnk_file}" -a "{wsl_exe_run_args(entry.Exec, args.shellcmd)}"')
